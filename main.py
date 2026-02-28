f15eb9d93b03f99b3fb32a375f3d0773922070bf
eitancoresh69-ctrl <eitancoresh69@gmail.com>
2026-02-28 13:02:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ app.add_middleware(
 
 SOFASCORE_BASE = "https://api.sofascore.com/api/v1"
 
-# תחפושת לדפדפן רגיל כדי לעקוף חסימות
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept": "*/*",
